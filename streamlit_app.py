964489fbd7eaccfb9255cd9de8620bbe8f226134
YLIANET\zong-han01273 <apple84026113@gmail.com>
2025-01-09 14:52:04 +0800

    add show_info method

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 VERSION_NUMBER = "V1.2.1"
 
+@st.dialog("⭕系統公告")
+def msg_content():
+    st.subheader("®️作者: HankLin") 
+    st.write("這是用於產製招標文件的工具")
+    st.markdown("---")
+    st.write("版本:"+VERSION_NUMBER )
+
 st.set_page_config(page_title=f"工程招標文件{VERSION_NUMBER}", page_icon="☕", layout="wide")
 
 # st.sidebar.title("工程招標文件處理工具V1.2")
@@ -11,5 +18,12 @@ bidform_page=st.Page("view_bidform.py",title="預算書審查",icon=":material/c
 biddoc_page=st.Page("view_biddoc.py",title="投標文件",icon=":material/assignment:")
 
 pg=st.navigation({"招標文件": [bidform_page,biddoc_page]})
+
+if "show_info" not in st.session_state:
+    st.session_state.show_info = True
+
+if st.session_state.show_info:
+    msg_content()
+    st.session_state.show_info = False
 
 pg.run()
