41da0ac10baf5aeb4c96564d6e9d2bf33d874d8d
YLIANET\zong-han01273 <apple84026113@gmail.com>
2025-01-09 15:43:55 +0800

    update default page

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ st.set_page_config(page_title=f"工程招標文件{VERSION_NUMBER}", page_icon="
 # st.sidebar.info("作者: HankLin")
 
 bidform_page=st.Page("view_bidform.py",title="預算書審查",icon=":material/contract:")
-biddoc_page=st.Page("view_biddoc.py",title="投標文件",icon=":material/assignment:")
+biddoc_page=st.Page("view_biddoc.py",title="投標文件",icon=":material/assignment:",default=True)
 
-pg=st.navigation({"招標文件": [bidform_page,biddoc_page]})
+pg=st.navigation([bidform_page,biddoc_page])
 
 if "show_info" not in st.session_state:
     st.session_state.show_info = True
