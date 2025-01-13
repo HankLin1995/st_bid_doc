31e0b5142284c48c4fa3fef4088b092862706b58
YLIANET\zong-han01273 <apple84026113@gmail.com>
2025-01-13 13:53:15 +0800

    update req

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/sql_app.db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DBPATH")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
