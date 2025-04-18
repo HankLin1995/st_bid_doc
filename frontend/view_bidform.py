0964a667e524bb3f4b013adf27189960cbddc511
YLIANET\zong-han01273 <apple84026113@gmail.com>
2025-04-18 13:23:28 +0800

    fix project id name to project number

--- frontend/view_bidform.py
+++ frontend/view_bidform.py
@@ -194,7 +194,7 @@ def budget_page():
                 st.balloons()
 
                 # Update project status and date
-                result = update_project_date_and_status(project_id, "預算書", datetime.now().strftime("%Y-%m-%d"))
+                result = update_project_date_and_status(project_number, "預算書", datetime.now().strftime("%Y-%m-%d"))
                 if result == "更新成功":
                     st.success("狀態更新成功!")
                 else:
@@ -210,11 +210,6 @@ def budget_page():
                 
                 if project:
                     st.warning("工程已存在，請勿重複創建!",icon="⚠️")
-                    result = update_project_date_and_status(project_number, "預算書", datetime.now().strftime("%Y-%m-%d"))
-                    if result == "更新成功":
-                        st.success("狀態更新成功!")
-                    else:
-                        st.error("狀態更新失敗!")
                 else:
                     st.error("創建失敗，請檢查資料是否正確")
 
